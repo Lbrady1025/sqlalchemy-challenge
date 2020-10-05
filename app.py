18e5950928ba4e52511d584bfa5ba09e440ebd27
Leah Brady <brady.leah@gmail.com>
2020-10-05 19:59:17 -0400

    add first query to app.py

--- app.py
+++ app.py
@@ -13,9 +13,23 @@ Base = automap_base()
 
 Base.prepare(engine, reflect=True)
 
+measurement = Base.classes.measurement
+station = Base.classes.station
+
 app = Flask(__name__)
 
 @app.route('/')
 def welcome():
     return(f"Welcome to the Hawaii Weather API!</br>"
             f"Available routes: </br>")
+
+@app.route('/api/v1.0/precipitation')
+def precipitation():
+
+
+@app.route('/api/v1.0/stations')
+def stations():
+
+
+@app.route('/api/v1.0/tobs')
+def temps():
