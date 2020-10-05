b446190269a1e2949942ba5f71dc3e74b186d36d
Leah Brady <brady.leah@gmail.com>
2020-10-04 20:38:25 -0400

    begin app.py file

---
+++ app.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+
+Base.prepare(engine, reflect=True)
+
